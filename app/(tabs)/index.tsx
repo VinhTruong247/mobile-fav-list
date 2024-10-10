a4f4fd81f712483f35716063bc565e4b9b4b119e
VinhTruong247 <roberttruong247@gmail.com>
2024-10-10 20:18:01 +0700

    Redesign Home UI & Clearing Bugs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, Image, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
 import { FontAwesome } from '@expo/vector-icons';
 import { useFavorites } from '@/context/FavoritesContext';
 import { useRouter } from 'expo-router';
@@ -14,13 +13,14 @@ type ArtTool = {
   limitedTimeDeal?: number;
 };
 
-type ApiResponse = ArtTool[]; // Define the expected type of the API response
+type ApiResponse = ArtTool[];
 
 export default function HomePage() {
   const { favorites, toggleFavorite } = useFavorites();
   const [artTools, setArtTools] = useState<ArtTool[]>([]);
   const [selectedBrand, setSelectedBrand] = useState<string>('All Brands');
   const [brands, setBrands] = useState<string[]>([]);
+  const [currentToolIndex, setCurrentToolIndex] = useState<number>(0);
   const router = useRouter();
 
   useEffect(() => {
@@ -30,6 +30,12 @@ export default function HomePage() {
         setArtTools(data);
         const uniqueBrands = Array.from(new Set(data.map((tool) => tool.brand)));
         setBrands(['All Brands', ...uniqueBrands]);
+
+        const interval = setInterval(() => {
+          setCurrentToolIndex((prevIndex) => (prevIndex + 1) % data.length);
+        }, 3000);
+
+        return () => clearInterval(interval);
       })
       .catch((error) => console.error('Error fetching art tools:', error));
   }, []);
@@ -38,44 +44,87 @@ export default function HomePage() {
     ? artTools
     : artTools.filter(tool => tool.brand === selectedBrand);
 
+  const listData = [
+    {
+      type: 'featured',
+      artTool: artTools[currentToolIndex],
+    },
+    ...filteredArtTools.map(tool => ({ type: 'artTool', artTool: tool })),
+  ];
+
+  const renderItem = ({ item }: { item: { type: string; artTool: ArtTool } }) => {
+    if (item.type === 'featured') {
+      return (
+        <View style={styles.featuredContainer}>
+          {item.artTool && (
+            <>
+              <Image source={{ uri: item.artTool.image }} style={styles.featuredImage} />
+              <Text style={styles.featuredArtName}>{item.artTool.artName}</Text>
+              <Text style={styles.featuredPrice}>${item.artTool.price}</Text>
+            </>
+          )}
+        </View>
+      );
+    } else {
+      const { artTool } = item;
+      return (
+        <TouchableOpacity onPress={() => router.push(`/detail/${artTool.id}`)}>
+          <View style={styles.artToolContainer}>           
+            <Image source={{ uri: artTool.image }} style={styles.artImage} />
+            <View style={styles.artDetails}>
+              <Text style={styles.artName}>{artTool.artName}</Text>
+              <Text style={styles.price}>${artTool.price}</Text>
+              {artTool.limitedTimeDeal !== undefined && artTool.limitedTimeDeal > 0 && (
+                <Text style={styles.dealText}>
+                  {Math.round(artTool.limitedTimeDeal * 100)}% OFF
+                </Text>
+              )}
+              <TouchableOpacity onPress={() => toggleFavorite(artTool)}>
+                <FontAwesome
+                  name={favorites.some(fav => fav.id === artTool.id) ? 'heart' : 'heart-o'}
+                  size={24}
+                  color={favorites.some(fav => fav.id === artTool.id) ? 'red' : 'gray'}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableOpacity>
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Picker
-        selectedValue={selectedBrand}
-        onValueChange={(itemValue) => setSelectedBrand(itemValue)}
-        style={styles.picker}
-      >
-        {brands.map(brand => (
-          <Picker.Item key={brand} label={brand} value={brand} />
-        ))}
-      </Picker>
+      <View style={styles.container_intro}>
+        <Text style={styles.intro_text_1}>EXPLORE</Text>
+        <Text style={styles.intro_text_2}>The Best Art Tools</Text>
+      </View>
+
+      <View style={{ marginTop: 8 }}>
+        <FlatList
+          horizontal
+          data={brands}
+          keyExtractor={(brand) => brand}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={[styles.brandButton, selectedBrand === item && styles.selectedBrandButton]}
+              onPress={() => setSelectedBrand(item)}
+            >
+              <Text style={[styles.brandButtonText, selectedBrand === item && styles.selectedBrandButtonText]}>
+                {item}
+              </Text>
+            </TouchableOpacity>
+          )}
+          contentContainerStyle={styles.brandSelector}
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
 
       <FlatList
-        data={filteredArtTools}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => router.push(`/detail/${item.id}`)}>
-            <View style={styles.artToolContainer}>
-              <Image source={{ uri: item.image }} style={styles.artImage} />
-              <View style={styles.artDetails}>
-                <Text style={styles.artName}>{item.artName}</Text>
-                <Text style={styles.price}>${item.price}</Text>
-                {item.limitedTimeDeal !== undefined && item.limitedTimeDeal > 0 && (
-                  <Text style={styles.dealText}>
-                    {Math.round(item.limitedTimeDeal * 100)}% OFF
-                  </Text>
-                )}
-                <TouchableOpacity onPress={() => toggleFavorite(item)}>
-                  <FontAwesome
-                    name={favorites.some(fav => fav.id === item.id) ? 'heart' : 'heart-o'}
-                    size={24}
-                    color={favorites.some(fav => fav.id === item.id) ? 'red' : 'gray'}
-                  />
-                </TouchableOpacity>
-              </View>
-            </View>
-          </TouchableOpacity>
-        )}
+        data={listData}
+        keyExtractor={(item, index) => item.type === 'featured' ? 'featured' : item.artTool.id}
+        renderItem={renderItem}
+        contentContainerStyle={styles.flatListContent}
       />
     </View>
   );
@@ -86,19 +135,73 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 16,
     backgroundColor: '#f7f7f7',
-    marginTop: 20,
   },
-  picker: {
-    height: 50,
-    width: '100%',
+  container_intro: {
+    marginTop: 50,
+    marginHorizontal: 30,
+  },
+  intro_text_1: {
+    fontSize: 40,
+    letterSpacing: -2,
+    fontWeight: "300",
+  },
+  intro_text_2: {
+    fontSize: 35,
+    fontWeight: "700",
+    letterSpacing: -2,
+  },
+  featuredContainer: {
+    marginHorizontal: 24,
+    marginBottom: 14,
+    padding: 20,
+    backgroundColor: '#e9ecef',
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  featuredImage: {
+    width: 120,
+    height: 120,
+    borderRadius: 8,
+    marginBottom: 8,
+  },
+  featuredArtName: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 4,
+  },
+  featuredPrice: {
+    color: '#555',
+    fontSize: 16,
+  },
+  brandSelector: {
+    flexDirection: 'row',
+    paddingVertical: 10,
+    borderRadius: 8,
     marginBottom: 16,
-    borderColor: '#ccc',
-    borderWidth: 1,
+    paddingHorizontal: 10,
+  },
+  brandButton: {
+    paddingVertical: 12,
+    paddingHorizontal: 20,
     borderRadius: 8,
     backgroundColor: '#fff',
+    marginRight: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 3,
   },
-  listContainer: {
-    paddingBottom: 16,
+  selectedBrandButton: {
+    backgroundColor: '#6200ee',
+  },
+  brandButtonText: {
+    fontSize: 16,
+    color: '#000',
+    fontWeight: '600',
+  },
+  selectedBrandButtonText: {
+    color: '#fff',
   },
   artToolContainer: {
     flexDirection: 'row',
@@ -134,5 +237,9 @@ const styles = StyleSheet.create({
     color: 'red',
     fontWeight: 'bold',
     fontSize: 14,
+  },
+  flatListContent: {
+    paddingTop: 8,
+    paddingBottom: 16,
   },
 });
--- app/detail/[id].tsx
+++ app/detail/[id].tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import { FontAwesome } from '@expo/vector-icons';
 import { useLocalSearchParams, useNavigation } from 'expo-router';
 import { useFavorites } from '@/context/FavoritesContext';
@@ -13,9 +14,18 @@ type ArtTool = {
   limitedTimeDeal?: number;
 };
 
+type Feedback = {
+  rating: number;
+  comment: string;
+  author: string;
+  date: string;
+};
+
 export default function ToolDetailPage() {
   const { id } = useLocalSearchParams();
   const [tool, setTool] = useState<ArtTool | null>(null);
+  const [feedbacks, setFeedbacks] = useState<Feedback[]>([]);
+  const [selectedRating, setSelectedRating] = useState<number | null>(null);
   const { favorites, toggleFavorite } = useFavorites();
   const navigation = useNavigation();
 
@@ -28,6 +38,58 @@ export default function ToolDetailPage() {
           navigation.setOptions({ title: data.artName });
         })
         .catch((error) => console.error('Error fetching tool details:', error));
+
+      setFeedbacks([
+        {
+          rating: 5,
+          comment: "Imagine all the watches, living in conFusion!",
+          author: "John Lemon",
+          date: "2023-10-16T17:57:28.556094Z",
+        },
+        {
+          rating: 4,
+          comment:
+            "Sends anyone to heaven, I wish I could get my mother-in-law to use it!",
+          author: "Paul McVites",
+          date: "2023-09-05T17:57:28.556094Z",
+        },
+        {
+          rating: 3,
+          comment: "Buy it, just buy it!",
+          author: "Michael Jaikishan",
+          date: "2024-02-13T17:57:28.556094Z",
+        },
+        {
+          rating: 4,
+          comment: "Ultimate, Reaching for the stars!",
+          author: "Ringo Starry",
+          date: "2023-12-02T17:57:28.556094Z",
+        },
+        {
+          rating: 2,
+          comment: "It's your birthday, we're gonna party!",
+          author: "25 Cent",
+          date: "2023-12-02T17:57:28.556094Z",
+        },
+        {
+          rating: 1,
+          comment: "Don't buy it!",
+          author: "Michael Shala",
+          date: "2023-12-11T17:57:28.556094Z",
+        },
+        {
+          rating: 2,
+          comment: "Poor stars!",
+          author: "Ringo Starry",
+          date: "2023-10-02T17:57:28.556094Z",
+        },
+        {
+          rating: 3,
+          comment: "Normal watch",
+          author: "25 Cent",
+          date: "2023-12-02T17:57:28.556094Z",
+        },
+      ]);
     }
   }, [id, navigation]);
 
@@ -36,9 +98,12 @@ export default function ToolDetailPage() {
   }
 
   const isFavorite = favorites.some((fav) => fav.id === tool.id);
+  const filteredFeedbacks = selectedRating
+    ? feedbacks.filter((feedback) => feedback.rating === selectedRating)
+    : feedbacks;
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Image source={{ uri: tool.image }} style={styles.image} />
       <View style={styles.detailsContainer}>
         <Text style={styles.artName}>{tool.artName}</Text>
@@ -60,7 +125,53 @@ export default function ToolDetailPage() {
           />
         </TouchableOpacity>
       </View>
-    </View>
+
+      <View style={styles.feedbackContainer}>
+        <Text style={styles.feedbackTitle}>User Feedback</Text>
+
+        <View style={styles.pickerContainer}>
+          <Text style={styles.pickerLabel}>Filter by rating:</Text>
+          <Picker
+            selectedValue={selectedRating}
+            style={styles.picker}
+            onValueChange={(itemValue) => {
+              setSelectedRating(itemValue === 'all' ? null : Number(itemValue));
+            }}
+          >
+            <Picker.Item label="All ratings" value="all" />
+            <Picker.Item label="5 Stars" value={5} />
+            <Picker.Item label="4 Stars" value={4} />
+            <Picker.Item label="3 Stars" value={3} />
+            <Picker.Item label="2 Stars" value={2} />
+            <Picker.Item label="1 Star" value={1} />
+          </Picker>
+        </View>
+
+        {filteredFeedbacks.length > 0 ? (
+          filteredFeedbacks.map((feedback, index) => (
+            <View key={index} style={styles.feedback}>
+              <View style={styles.ratingContainer}>
+                {Array.from({ length: 5 }, (_, i) => (
+                  <FontAwesome
+                    key={i}
+                    name={i < feedback.rating ? 'star' : 'star-o'}
+                    size={18}
+                    color={i < feedback.rating ? 'gold' : 'gray'}
+                  />
+                ))}
+              </View>
+              <Text style={styles.comment}>"{feedback.comment}"</Text>
+              <Text style={styles.author}>- {feedback.author}</Text>
+              <Text style={styles.date}>
+                {new Date(feedback.date).toLocaleDateString()}
+              </Text>
+            </View>
+          ))
+        ) : (
+          <Text style={styles.noFeedbackText}>No feedback available for this rating.</Text>
+        )}
+      </View>
+    </ScrollView>
   );
 }
 
@@ -77,7 +188,6 @@ const styles = StyleSheet.create({
     marginBottom: 16,
   },
   detailsContainer: {
-    flex: 1,
     padding: 16,
     backgroundColor: '#fff',
     borderRadius: 10,
@@ -86,6 +196,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 4,
     elevation: 3,
+    marginBottom: 16,
   },
   artName: {
     fontWeight: 'bold',
@@ -112,5 +223,57 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     marginTop: 20,
+  },
+  feedbackContainer: {
+    marginTop: 20,
+    padding: 16,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  feedbackTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  pickerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 16,
+  },
+  pickerLabel: {
+    fontSize: 16,
+    marginRight: 8,
+  },
+  picker: {
+    flex: 1,
+    height: 40,
+  },
+  feedback: {
+    marginBottom: 16,
+  },
+  comment: {
+    fontStyle: 'italic',
+    marginBottom: 4,
+  },
+  author: {
+    fontWeight: 'bold',
+    marginBottom: 2,
+  },
+  date: {
+    color: '#999',
+    fontSize: 12,
+  },
+  noFeedbackText: {
+    textAlign: 'center',
+    color: '#999',
+  },
+   ratingContainer: {
+    flexDirection: 'row',
+    marginBottom: 4,
   },
 });
